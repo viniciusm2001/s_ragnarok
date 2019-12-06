0fcf29309aa101e98991f29c24ab5f8b7797e416
Vinicius M <vini@meuemail.com>
2019-12-05 22:42:45 -0300

    isudisyh

---
+++ precache-manifest.d039d21d926dd413bb208bdf1cf3f3c3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d455c913f464219356a575e36391e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ffe8c842dc3bb747e1",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a5b218c11c187ee065cd",
+    "url": "/static/css/main.3d15b1bf.chunk.css"
+  },
+  {
+    "revision": "b3ffe8c842dc3bb747e1",
+    "url": "/static/js/2.798b47b8.chunk.js"
+  },
+  {
+    "revision": "a5b218c11c187ee065cd",
+    "url": "/static/js/main.d3920382.chunk.js"
+  },
+  {
+    "revision": "99e838173ff38da11f00",
+    "url": "/static/js/runtime-main.a634196b.js"
+  },
+  {
+    "revision": "c8309030f6e9cdd796ef53587b1c88f8",
+    "url": "/static/media/GoboldBlockyItalic.c8309030.otf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "bf686e7dec8118c9e51a9613f74c98cb",
+    "url": "/static/media/acessorio.bf686e7d.png"
+  },
+  {
+    "revision": "df5684797d49011250506fdb1c2335aa",
+    "url": "/static/media/bell.df568479.png"
+  },
+  {
+    "revision": "30eba7c38c323d50851e381d29715dc3",
+    "url": "/static/media/console.30eba7c3.png"
+  },
+  {
+    "revision": "df566969da5eb0c3ede82b30bc8604f3",
+    "url": "/static/media/consolesbackground1cortado.df566969.png"
+  },
+  {
+    "revision": "1309d0066e4aaa7b3020ac97953fe821",
+    "url": "/static/media/cursor.1309d006.png"
+  },
+  {
+    "revision": "22dd93e489859a16cf96ad96934eaae2",
+    "url": "/static/media/god-of-war.22dd93e4.jpg"
+  },
+  {
+    "revision": "f8fcea5887a95121a239f5c20e55bdb3",
+    "url": "/static/media/jogo.f8fcea58.png"
+  },
+  {
+    "revision": "deac1d8f7f777770e0dbe069be929867",
+    "url": "/static/media/jumpforce.deac1d8f.jpg"
+  },
+  {
+    "revision": "65ae5a465d37c870d456929d08ccd14b",
+    "url": "/static/media/logo-ragnarok.65ae5a46.png"
+  },
+  {
+    "revision": "32e61a706e4894918ffbd20ea1fa9dc3",
+    "url": "/static/media/testejogo1.32e61a70.jpg"
+  },
+  {
+    "revision": "8aca3561b14ca07f10044e7d58b71cd6",
+    "url": "/static/media/user.8aca3561.svg"
+  }
+]);
